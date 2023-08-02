2233aea6959c2490205cd1961f51825f6f00bc24
Alexey Nurgaliev <atnurgaliev@gmail.com>
2023-08-02 22:28:12 +0300

    load state modal

--- assets/state.js
+++ assets/state.js
@@ -1,14 +1,8 @@
 (() => {
 
-    EJS_onSaveState = e => {
-        uploadState(e.state, e.screenshot)
-    };
-
-    EJS_onGameStart = () => {
-        console.log('game started');
-    };
-
     window.addEventListener('load', () => {
+        const loadStateModal = new bootstrap.Modal(document.getElementById('modal-load-state'));
+
         document.getElementById('btn-save-state').addEventListener('click', async () => {
             const state = await saveState();
             if (state !== null) {
@@ -21,6 +15,12 @@
                 return;
             }
             await loadState(window.LatestStateUrl);
+        });
+
+        document.getElementById('btn-load-state').addEventListener('click', async () => {
+            const states = await listStates();
+            renderSaveStates(states, loadStateModal);
+            loadStateModal.show();
         });
     });
 
@@ -69,6 +69,52 @@
             console.error("list states error", e);
             return [];
         }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    function renderSaveStates(states, modal) {
+        const stateList = document.getElementById('load-state-list');
+        stateList.innerHTML = '';
+
+        const emptyState = document.getElementById('load-state-empty');
+
+        if (!states || states.length === 0) {
+            emptyState.classList.remove('d-none');
+            return;
+        } else {
+            emptyState.classList.add('d-none');
+        }
+
+        states.forEach(state => {
+            const container = document.createElement('div');
+            container.classList.add('col-4');
+
+            const card = document.createElement('div');
+            card.classList.add('card', 'mb-3');
+
+            const img = document.createElement('img');
+            img.classList.add('card-img-top')
+            img.src = state.ScreenshotDownloadLink;
+            img.alt = 'Screenshot';
+
+            const body = document.createElement('div');
+            body.classList.add('card-body', 'text-center');
+
+            const button = document.createElement('button');
+            button.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+            button.type = 'button';
+            button.innerText = new Date(state.Created).toLocaleString();
+            button.onclick = () => {
+                modal.hide();
+                loadState(state.StateFileDownloadLink).then();
+            };
+
+            body.append(button);
+            card.append(img, body);
+            container.append(card);
+            stateList.append(container);
+        });
     }
 
 })();
