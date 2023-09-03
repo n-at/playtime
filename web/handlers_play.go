000aa3efc96e1f36e3435e90e72aa122843a61a9
Alexey Nurgaliev <atnurgaliev@gmail.com>
2023-09-03 16:48:36 +0300

    if netplay is enabled, disable volume controls for host

--- web/handlers_play.go
+++ web/handlers_play.go
@@ -57,6 +57,14 @@ func (s *Server) play(c echo.Context) error {
 	c.Response().Header().Add("Cross-Origin-Opener-Policy", "same-origin")
 	c.Response().Header().Add("Cross-Origin-Embedder-Policy", "require-corp")
 
+	netplayEnabled := s.config.NetplayEnabled && game.NetplayEnabled && len(game.NetplaySessionId) != 0
+
+	if netplayEnabled {
+		emulatorSettings.Volume = 1.0
+		emulatorSettings.Buttons.Volume = false
+		emulatorSettings.Buttons.Mute = false
+	}
+
 	return c.Render(http.StatusOK, "play", pongo2.Context{
 		"_csrf_token":           c.Get("csrf"),
 		"user":                  context.user,
@@ -66,7 +74,7 @@ func (s *Server) play(c echo.Context) error {
 		"bios":                  bios,
 		"save_state":            saveState,
 		"emulator_debug":        s.config.EmulatorDebug,
-		"netplay_enabled":       s.config.NetplayEnabled && game.NetplayEnabled && len(game.NetplaySessionId) != 0,
+		"netplay_enabled":       netplayEnabled,
 		"netplay_debug":         s.config.NetplayDebug,
 		"netplay_turn_url":      s.config.TurnServerUrl,
 		"netplay_turn_user":     s.config.TurnServerUser,
