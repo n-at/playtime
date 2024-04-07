a67d522e4050fed6ae2c2733936619ab3e9d3948
Alexey Nurgaliev <atnurgaliev@gmail.com>
2024-04-07 21:27:59 +0300

    tags search

--- assets/games-search.js
+++ assets/games-search.js
@@ -1,13 +1,59 @@
+import Tags from '/assets/node_modules/bootstrap5-tags/tags.js'
+
 (() => {
 
+    let searchField = null;
+    let tagsField = null;
+
     window.addEventListener('load', () => {
-        const searchField = document.getElementById('games-search');
-        searchField.addEventListener('keyup', () => {
-            search(searchField.value);
+        searchField = document.getElementById('games-search');
+        searchField.addEventListener('keyup', doSearch);
+
+        Tags.init('#games-tags');
+        const tagsEl = document.getElementById('games-tags');
+        tagsField = Tags.getInstance(tagsEl);
+        tagsField.setConfig('onSelectItem', doSearch);
+        tagsField.setConfig('onClearItem', doSearch);
+
+        document.querySelectorAll('span.game-tag').forEach(tagEl => {
+            const tagValue = tagEl.innerText.trim();
+            tagEl.addEventListener('click', () => {
+                if (!tagsField.getSelectedValues().includes(tagValue)) {
+                    tagsField.addItem(tagValue);
+                    doSearch();
+                }
+            });
         });
+
+        loadSearchParams();
     });
 
-    function search(text) {
+    function doSearch() {
+        const searchText = searchField.value;
+        const searchTags = tagsField.getSelectedValues();
+
+        search(searchText, searchTags);
+
+        if (window.sessionStorage) {
+            sessionStorage._games_search_text = searchText;
+            sessionStorage._games_search_tags = JSON.stringify(searchTags);
+        }
+    }
+
+    function loadSearchParams() {
+        if (!window.sessionStorage) {
+            return;
+        }
+        if (window.sessionStorage._games_search_text) {
+            searchField.value = window.sessionStorage._games_search_text;
+        }
+        if (window.sessionStorage._games_search_tags) {
+            JSON.parse(window.sessionStorage._games_search_tags).forEach(tag => tagsField.addItem(tag));
+        }
+        doSearch();
+    }
+
+    function search(text, tags) {
         const searchText = escapeRegExpChars(text.trim());
         const re = new RegExp(searchText, 'ig');
 
@@ -15,7 +61,18 @@
 
         document.querySelectorAll('div.game').forEach(gameEl => {
             const gameName = gameEl.querySelector('a.game-name').innerText;
-            if (!text || gameName.match(re)) {
+            const textMatched = !text || gameName.match(re);
+
+            let tagsCount = 0;
+            gameEl.querySelectorAll('span.game-tag').forEach(tagEl => {
+                const tag = tagEl.innerText.trim();
+                if (tags.includes(tag)) {
+                    tagsCount++;
+                }
+            });
+            let tagsMatched = !tags || tags.length === 0 || tagsCount === tags.length;
+
+            if (textMatched && tagsMatched) {
                 gameEl.classList.remove('d-none');
                 gameFound = true;
             } else {
