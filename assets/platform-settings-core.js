f9070dd4abc064cadfeec71177a13ffb33102909
Alexey Nurgaliev <atnurgaliev@gmail.com>
2024-04-08 09:22:42 +0300

    layout and style updates

--- assets/platform-settings-core.js
+++ assets/platform-settings-core.js
@@ -52,9 +52,19 @@
         }
     }
 
+    function showCoreOptions() {
+        const value = document.getElementById('core').value;
+        document.querySelectorAll('.core-options').forEach(it => it.classList.add('d-none'));
+        document.getElementById(`core-options-${value}`).classList.remove('d-none');
+    }
+
     window.addEventListener('load', () => {
-        document.getElementById('core').addEventListener('change', setupThreadsControl);
+        document.getElementById('core').addEventListener('change', () => {
+            setupThreadsControl();
+            showCoreOptions();
+        });
         setupThreadsControl();
+        showCoreOptions();
     });
 
 })();
