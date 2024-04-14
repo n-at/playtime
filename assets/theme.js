a8734cb265d60bf73ddc3b7cf1acd12010cf9e55
Alexey Nurgaliev <atnurgaliev@gmail.com>
2024-04-14 15:12:34 +0300

    page footer, theme selector, system color theme

--- assets/theme.js
+++ assets/theme.js
@@ -1,29 +1,67 @@
 (() => {
 
-    const darkModeSwitch = document.getElementById('dark-mode');
-    darkModeSwitch.checked = getCurrentColorScheme() === 'dark';
-    darkModeSwitch.onchange = () => {
-        const theme = darkModeSwitch.checked ? 'dark' : 'light';
-        applyDarkTheme(theme);
-    };
-    applyDarkTheme(getCurrentColorScheme());
+    const ThemeSystemDefault = 'system';
+    const ThemeLight = 'light';
+    const ThemeDark = 'dark';
 
-    function applyDarkTheme(theme) {
+    let currentTheme = ThemeSystemDefault;
+
+    function applyTheme(theme) {
         const html = document.getElementsByTagName('html')[0];
         html.setAttribute('data-bs-theme', theme);
-
-        window.localStorage.__dark_theme = theme;
     }
 
-    function getCurrentColorScheme() {
-        const theme = window.localStorage.__dark_theme;
-        if (!theme) {
-            return isSystemDarkMode() ? 'dark' : 'light';
+    function setCurrentTheme(theme) {
+        currentTheme = theme;
+        window.localStorage.__theme = theme;
+        if (theme === ThemeSystemDefault) {
+            applyTheme(isSystemDarkMode() ? ThemeDark : ThemeLight);
+        } else {
+            applyTheme(theme);
         }
-        return theme;
+
+        const themeSwitcherIconEl = document.querySelector('#theme i.bi');
+        themeSwitcherIconEl.classList.remove('bi-circle-half', 'bi-sun', 'bi-moon-stars');
+        switch (theme) {
+            case ThemeSystemDefault:
+                themeSwitcherIconEl.classList.add('bi-circle-half');
+                break;
+            case ThemeLight:
+                themeSwitcherIconEl.classList.add('bi-sun');
+                break;
+            case ThemeDark:
+                themeSwitcherIconEl.classList.add('bi-moon-stars');
+                break;
+        }
+    }
+
+    function getCurrentTheme() {
+        const theme = window.localStorage.__theme;
+        return theme ? theme : ThemeSystemDefault;
     }
 
     function isSystemDarkMode() {
         return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
     }
+
+    window.addEventListener('load', () => {
+        document.getElementById('theme-system-default').addEventListener('click', () => {
+            setCurrentTheme(ThemeSystemDefault);
+        });
+        document.getElementById('theme-light').addEventListener('click', () => {
+            setCurrentTheme(ThemeLight);
+        });
+        document.getElementById('theme-dark').addEventListener('click', () => {
+            setCurrentTheme(ThemeDark);
+        });
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+            const newTheme = event.matches ? ThemeDark : ThemeLight;
+            if (currentTheme === ThemeSystemDefault) {
+                applyTheme(newTheme);
+            }
+        });
+    });
+
+    setCurrentTheme(getCurrentTheme());
 })();
